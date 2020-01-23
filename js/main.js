0a748a517c2a5b072fcd3695acd237e1dc35702b
Naman-sopho <namantiwari.nt@gmail.com>
2020-01-23 14:14:30 -0500

    Resolve wow.js bug
    Implement scrolling from navbar links

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 function scroll_to(clicked_link, nav_height) {
     var element_class = clicked_link.attr('href').replace('#', '.');
     var scroll_to = 0;
-    if (element_class != '.top-content') {
+
+    if (element_class != '.top') {
         element_class += '-container';
         scroll_to = $(element_class).offset().top - nav_height;
     }
+
     if ($(window).scrollTop() != scroll_to) {
         $('html, body').stop().animate({ scrollTop: scroll_to }, 1000);
     }
@@ -16,15 +18,23 @@ $(document).ready(function () {
         scroll_to($(this), $('nav').outerHeight());
     });
 
-
-    // Initialise WoW
-    new WOW().init();
-
     $('.top').backstretch("images/llama-top-bg.jpg");
     $('.facts-container').backstretch("images/llama-top-bg.jpg");
 
-    // crousel
+    // carousel
+
+    $('.nav-tabs a').click(function () {
+        $(this).tab('show');
+    });
+
     $('a[data-toggle="tab"]').on('shown.bs.tab', function () {
         $('.facts-container').backstretch("resize");
     });
+
+    // Initialise WoW
+    new WOW().init();
+});
+
+jQuery(window).on("load", function () {
+    $(".fact-image img").attr("style", "width: auto !important; height: auto !important;");
 });
